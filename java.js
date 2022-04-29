a78535b07224c18a9cd8b9fcb23f61db9ae56957
Yoana Arias-Magana <yoanaarias-magana@Administrators-MacBook-Pro.local>
2022-04-29 14:42:12 -0400

    first commit

---
+++ java.js
@@ -0,0 +1,22 @@
+const arrowMyFunction = (num1, num2) => (num1 + num2);
+//console logging it to make sure it works
+console.log(arrowMyFunction(9, 10));
+
+
+
+const arrowAdd100 = money => money + 100;
+//console logging to make sure it works
+console.log(arrowAdd100(100));
+
+
+
+
+const findCharacters = (string, letter) => "Hey how's it going".search("h");
+
+
+
+
+
+
+
+
